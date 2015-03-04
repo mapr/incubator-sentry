b562e3b29f807d6e8452a46324e4488c5498003c
Oleksiy Sayankin <osayankin@maprtech.com>
2015-03-04 13:46:22 +0200

    Refactored sentry-daemon.sh: used quotes and $().

--- bin/sentry-daemon.sh
+++ bin/sentry-daemon.sh
@@ -18,77 +18,76 @@
 USAGE="Usage: sentry-daemon.sh \
  (start <configuration file>|stop|status)"
 
-PATH_BIN=`dirname "${BASH_SOURCE-$0}"`
+PATH_BIN=$(dirname "${BASH_SOURCE-$0}")
 
-SENTRY_PATH=$PATH_BIN/..
-PATH_LOG=$SENTRY_PATH/logs
-PATH_CONF=$SENTRY_PATH/conf
+SENTRY_PATH=${PATH_BIN}/..
+PATH_LOG=${SENTRY_PATH}/logs
+PATH_CONF=${SENTRY_PATH}/conf
 WARDEN_SENTRY_CONF=/opt/mapr/conf/conf.d/warden.sentry.conf
-SENTRY_THRIFT_PID_FILE=/opt/mapr/pid/sentry-thrift.pid
 DEFAULT_SENTRY_PORT=8038
 
 find_sentry_port(){
-if [ -f $WARDEN_SENTRY_CONF ]; then
-    local SENTRY_PORT=`sed -n "/service.port=/p" $WARDEN_SENTRY_CONF`
-    echo `echo $SENTRY_PORT | awk -F "=" '{print $2}'`
+if [ -f ${WARDEN_SENTRY_CONF} ]; then
+    local SENTRY_PORT=$(sed -n "/service.port=/p" ${WARDEN_SENTRY_CONF})
+    echo "$(echo "$SENTRY_PORT" | awk -F "=" '{print $2}')"
 else
-    echo $DEFAULT_SENTRY_PORT
+    echo "$DEFAULT_SENTRY_PORT"
 fi
 }
 
 find_sentry_pid(){
 local SENTRY_PORT=$(find_sentry_port)
-local SENTRY_PID=`lsof -i:$SENTRY_PORT | grep LISTEN | awk '{print $2}'`
+local SENTRY_PID=$(lsof -i:"$SENTRY_PORT" | grep LISTEN | awk '{print $2}')
 if [ -n "$SENTRY_PID" ]; then
-    echo $SENTRY_PID
+    echo "$SENTRY_PID"
     else echo 0
 fi
 }
 
 if [ $# -lt 1 ]; then
-    echo $USAGE
+    echo "$USAGE"
     exit 1
 fi
 
 COMMAND=$1
 CONF_FILE=$2
 
-if [[ $COMMAND == 'start' && -z $CONF_FILE ]]; then
+if [[ "$COMMAND" == 'start' && -z "$CONF_FILE" ]]; then
     echo 'Error: No configuration file is set!'
-    echo $USAGE
+    echo "$USAGE"
     exit 1
 fi
 
 
-case $COMMAND in
+case ${COMMAND} in
  status)
      SENTRY_PROCESS_ID=$(find_sentry_pid)
-     if [ $SENTRY_PROCESS_ID -gt 0 ];then
-         echo Sentry is running as process $SENTRY_PROCESS_ID.
-         echo " `date +%m.%d-%H:%M:%S` INFO : Process checked for sentry : TRUE" >> $PATH_LOG/daemons.txt
+     if [ "$SENTRY_PROCESS_ID" -gt 0 ];then
+         echo "Sentry is running as process $SENTRY_PROCESS_ID."
+         echo "$(date +%m.%d-%H:%M:%S) INFO : Process checked for sentry : TRUE" >> "$PATH_LOG/daemons.txt"
          exit 0;
      else
-         echo Sentry is not running.
-         echo " `date +%m.%d-%H:%M:%S` INFO : Process checked for sentry : FALSE" >> $PATH_LOG/daemons.txt
+         echo "Sentry is not running."
+         echo "$(date +%m.%d-%H:%M:%S) INFO : Process checked for sentry : FALSE" >> "$PATH_LOG/daemons.txt"
          exit 1;
      fi
  ;;
  stop)
      SENTRY_PROCESS_ID=$(find_sentry_pid)
-     if [ $SENTRY_PROCESS_ID -gt 0 ];then
-         kill -9 $SENTRY_PROCESS_ID
+     if [ "$SENTRY_PROCESS_ID" -gt 0 ];then
+         kill -9 "$SENTRY_PROCESS_ID"
          exit 0
      else
-         echo Sentry is not running.
+         echo "Sentry is not running."
      fi
  ;;
  start)
      SENTRY_PROCESS_ID=$(find_sentry_pid)
-     if [ $SENTRY_PROCESS_ID -gt 0 ];then
+     if [ "$SENTRY_PROCESS_ID" -gt 0 ];then
          echo "Sentry is already running with PID $SENTRY_PROCESS_ID. Stop it first."
      else
-         echo " `date +%m.%d-%H:%M:%S` INFO : Process starting for sentry " >> $PATH_LOG/daemons.txt
-         nohup ${PATH_BIN}/sentry --log4jConf ${PATH_CONF}/log4j.properties --command service -c ${CONF_FILE} > $PATH_LOG/nohup.out 2>&1 </dev/null &
-         echo " `date +%d-%H.%M.%S` INFO : Process started for sentry " >> $PATH_LOG/daemons.txt
+         echo "$(date +%m.%d-%H:%M:%S) INFO : Process starting for sentry " >> "$PATH_LOG/daemons.txt"
+         nohup "${PATH_BIN}/sentry" --log4jConf "${PATH_CONF}/log4j.properties" --command service -c "${CONF_FILE}" > "$PATH_LOG/nohup.out" 2>&1 </dev/null &
+         echo "$(date +%d-%H.%M.%S) INFO : Process started for sentry " >> "$PATH_LOG/daemons.txt"
      fi
 esac
